aa03528de1b2000aaffee731d3e31aa6e854a588
Brian Fink <bfink@indeed.com>
2019-04-01 08:59:28 -0500

    Add encrypted docker creds to travis file. Push to docker when a tag exists and we aren't in a pull request

--- release.sh
+++ release.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-docker build -t indeedoss/rabbitmq-operator:latest .
+docker build -t indeedeng/rabbitmq-operator:latest .
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "built pull request, nothing left to do"
 elif [[ ! -z "$TRAVIS_TAG" ]]; then
     echo "tag and push docker"
-    docker tag indeedoss/rabbitmq-operator:latest indeedoss/rabbitmq-operator:$TRAVIS_TAG
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker tag indeedeng/rabbitmq-operator:latest indeedeng/rabbitmq-operator:$TRAVIS_TAG
+    docker push indeedeng/rabbitmq-operator:$TRAVIS_TAG
 else
     echo "nothing to do"
 fi
