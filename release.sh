ff4044df7394ad1e7be536ec287f7be1fbce337e
Brian Fink <bfink@indeed.com>
2019-04-01 10:54:49 -0500

    Push latest tag to dockerhub in addition to versioned tag

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ elif [[ ! -z "$TRAVIS_TAG" ]]; then
     echo "tag and push docker"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker tag indeedoss/rabbitmq-operator:latest indeedoss/rabbitmq-operator:$TRAVIS_TAG
+    docker push indeedoss/rabbitmq-operator:latest
     docker push indeedoss/rabbitmq-operator:$TRAVIS_TAG
 else
     echo "nothing to do"
